41847c3b1714fc903088cf4a70f0e92f235c5979
sellysyifa <83294078+sellysyifa@users.noreply.github.com>
2021-10-25 09:41:02 +0700

    Initial commit

---
+++ app/src/main/java/com/project/projectappmatkulmobile/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.project.projectappmatkulmobile
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<Button>(R.id.btn1)
+        val btn2 = findViewById<Button>(R.id.btn2)
+        val tv1 = findViewById<TextView>(R.id.tv1)
+
+
+        btn2.setOnClickListener {
+            val intent = Intent(this,SecondActivity :: class.java)
+            startActivity(intent)
+        }
+
+        btn1.setOnClickListener {
+            Toast.makeText(this,"Ini proses dari klik tombol", Toast.LENGTH_SHORT).show()
+            tv1.text = "Berubah setelah tombol di kKlik"
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/project/projectappmatkulmobile/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.project.projectappmatkulmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val bar = supportActionBar
+        bar!!.title = "Second Activiy"
+
+        bar.setDisplayHomeAsUpEnabled(true)
+    }
+}
