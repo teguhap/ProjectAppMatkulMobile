4f440365f48ddb3c5aee568b4ac2c5c6cc94b48c
teguhap <61332266+teguhap@users.noreply.github.com>
2021-12-05 23:32:51 +0700

    Pertemuan 10 Input Data Mahasiswa Ke Database

--- app/src/main/java/com/project/projectappmatkulmobile/SecondActivity.kt
+++ app/src/main/java/com/project/projectappmatkulmobile/SecondActivity.kt
@@ -3,7 +3,15 @@ package com.project.projectappmatkulmobile
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.*
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import org.json.JSONException
+import org.json.JSONObject
 
 class SecondActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
@@ -61,8 +69,47 @@ class SecondActivity : AppCompatActivity() {
 
             tvDataMhs.text = " Nama\t: $nama\n Npm\t: $npm\n JenisKelamin\t: $jenisKelamin\n Prodi\t: $prodi\n Hobi\t: $hobi"
 
-        }
 
+            val BASE_URL = "http://192.168.43.55/kuliah_mobile_prog_unsika/index.php?op="
+            val ACTION = BASE_URL+"mahasiswa_create&npm='$npm'&nama='$nama'&jenis_kelamin='$jenisKelamin'&prodi='$prodi'&hobi='$hobi'"
+
+
+
+            val stringRequest = object : StringRequest(Request.Method.GET,ACTION,
+            Response.Listener<String>{ response ->
+                try{
+                    val obj = JSONObject(response)
+                    Log.i("hasil",obj.getString("message"))
+                }catch(e: JSONException){
+                    e.printStackTrace()
+                }
+            },
+             object : Response.ErrorListener{
+                override fun onErrorResponse(error: VolleyError?) {
+                    Log.e(
+                        "hasil : ",error!!.message.toString()
+                    )
+                }
+            }) {
+                @Throws(AuthFailureError::class)
+                override fun getParams(): MutableMap<String, String> {
+                    val params = HashMap<String,String>()
+                    return params
+                }
+            }
+
+            Sender.instance!!.addToRequestQueue(stringRequest)
+
+            Toast.makeText(this,"Data Berhasil Terinput",Toast.LENGTH_SHORT).show()
+
+
+            etNama.text.clear()
+            etNpm.text.clear()
+            rgjenisKelamin.check(R.id.rbLaki)
+            spProdi.setSelection(0)
+            cbBacaBuku.isChecked = false
+            cbPemrograman.isChecked = false
+        }
 
     }
 }
